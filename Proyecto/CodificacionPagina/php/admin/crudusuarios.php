33c6089c6ff98df1b187fe71ad86334f734f957e
PedroPollo <72528531+PedroPollo@users.noreply.github.com>
2022-12-23 12:34:34 -0600

    de chil

--- Proyecto/CodificacionPagina/php/admin/crudusuarios.php
+++ Proyecto/CodificacionPagina/php/admin/crudusuarios.php
@@ -3,8 +3,15 @@
 $conexion=mysqli_connect('localhost','root','','proyecto');
 
 $username = (isset($_POST['username'])) ? $_POST['username'] : '';
+<<<<<<< Updated upstream
 $rol = (isset($_POST['rol'])) ? $_POST['rol'] : '';
+=======
+$nombre = (isset($_POST['nombre'])) ? $_POST['nombre'] : '';
+$ap_paterno = (isset($_POST['ap_paterno'])) ? $_POST['ap_paterno'] : '';
+$ap_materno = (isset($_POST['ap_materno'])) ? $_POST['ap_materno'] : '';
+>>>>>>> Stashed changes
 $password = (isset($_POST['password'])) ? $_POST['password'] : '';
+$rol = (isset($_POST['rol'])) ? $_POST['rol'] : '';
 
 
 $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
@@ -13,6 +20,7 @@ $user_id = (isset($_POST['user_id'])) ? $_POST['user_id'] : '';
 
 switch($opcion){
     case 1:
+<<<<<<< Updated upstream
         $consulta = "INSERT INTO usuarios (`usuarios_nombre`,`usuarios_pass`,`usuarios_rol_id`) VALUES('$username', '$password', '$rol') ";			
         $datos = mysqli_query($conexion, $consulta);
         $resultado = mysqli_fetch_all($datos);
@@ -22,6 +30,26 @@ switch($opcion){
         $consulta = "UPDATE usuarios SET usuarios_nombre='$username', rol='$rol', password='$password' WHERE usuarios_id='$user_id' ";		
         $datos = mysqli_query($conexion, $consulta);
         echo json_encode($datos);
+=======
+        $consulta = "INSERT INTO usuarios (username, nombre, ap_paterno, ap_materno, password, rol) VALUES('$username', '$nombre', '$ap_paterno', '$ap_materno', '$password', '$rol') ";			
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute(); 
+        
+        $consulta = "SELECT * FROM usuarios ORDER BY user_id DESC LIMIT 1";
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);       
+        break;    
+    case 2:        
+        $consulta = "UPDATE usuarios SET username='$username', nombre='$nombre', ap_paterno='$ap_paterno', ap_materno='$ap_materno', password='$password', rol='$rol' WHERE id='$user_id' ";		
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();        
+        
+        $consulta = "SELECT * FROM usuarios WHERE id='$user_id' ";       
+        $resultado = $conexion->prepare($consulta);
+        $resultado->execute();
+        $data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+>>>>>>> Stashed changes
         break;
     case 3:        
         $consulta = "DELETE FROM usuarios WHERE usuarios_id='$user_id' ";		
---
+++ Proyecto/CodificacionPagina/php/admin/usuarios.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+    var user_id, opcion;
+    opcion = 4;
+        
+    tablaUsuarios = $('#tablaUsuarios').DataTable({  
+        "ajax":{            
+            "url": "bd/crud.php", 
+            "method": 'POST', //usamos el metodo POST
+            "data":{opcion:opcion}, //enviamos opcion 4 para que haga un SELECT
+            "dataSrc":""
+        },
+        "columns":[
+            {"data": "id"},
+            {"data": "username"},
+            {"data": "password"},
+            {"data": "nombre"},
+            {"data": "ap_paterno"},
+            {"data": "ap_materno"},
+            {"data": "rol"},
+            {"defaultContent": "<div class='btn-group' role='group' aria-label='Basic mixed styles example'><button type='button' class='btn btn-danger btnBorrar'>Eliminar</button><button type='button' class='btn btn-warning btnEditar'>Editar</button>"}
+        ]
+    });     
+    
+    var fila; //captura la fila, para editar o eliminar
+    //submit para el Alta y Actualización
+    $('#formUsuarios').submit(function(e){                         
+        e.preventDefault(); //evita el comportambiento normal del submit, es decir, recarga total de la página
+        username = $.trim($('#username').val());    
+        password = $.trim($('#password').val());
+        nombre = $.trim($('#nombre').val());    
+        ap_paterno = $.trim($('#ap_paterno').val());    
+        ap_materno = $.trim($('#ap_materno').val());
+        rol = $.trim($('#rol').val());                            
+            $.ajax({
+              url: "bd/crud.php",
+              type: "POST",
+              datatype:"json",    
+              data:  {user_id:user_id, username:username, password:password, nombre:nombre, ap_paterno:ap_paterno, ap_materno:ap_materno ,rol:rol ,opcion:opcion},    
+              success: function(data) {
+                tablaUsuarios.ajax.reload(null, false);
+               }
+            });			        
+        $('#modalCRUD').modal('hide');											     			
+    });
+            
+     
+    
+    //para limpiar los campos antes de dar de Alta una Persona
+    $("#btnNuevo").click(function(){
+        opcion = 1; //alta           
+        user_id=null;
+        $("#formUsuarios").trigger("reset");
+        $(".modal-header").css( "background-color", "#fff");
+        $(".modal-header").css( "color", "black" );
+        $(".modal-title").text("Alta de Usuario");
+        $('#modalCRUD').modal('show');	    
+    });
+    
+    //Editar        
+    $(document).on("click", ".btnEditar", function(){		        
+        opcion = 2;//editar
+        fila = $(this).closest("tr");	        
+        user_id = parseInt(fila.find('td:eq(0)').text()); //capturo el ID		            
+        username = fila.find('td:eq(1)').text();
+        password = fila.find('td:eq(2)').text();
+        nombre = fila.find('td:eq(3)').text();
+        ap_paterno = fila.find('td:eq(4)').text();
+        ap_materno = fila.find('td:eq(5)').text();
+        rol = fila.find('td:eq(6)').text();
+        $("#username").val(username);
+        $("#password").val(password);
+        $("#nombre").val(nombre);
+        $("#ap_paterno").val(ap_paterno);
+        $("#ap_materno").val(ap_materno);
+        $("#rol").val(rol);
+        $(".modal-header").css("background-color", "#fff");
+        $(".modal-header").css("color", "black" );
+        $(".modal-title").text("Editar Usuario");		
+        $('#modalCRUD').modal('show');		   
+    });
+    
+    //Borrar
+    $(document).on("click", ".btnBorrar", function(){
+        fila = $(this);           
+        user_id = parseInt($(this).closest('tr').find('td:eq(0)').text()) ;		
+        opcion = 3; //eliminar        
+        var respuesta = confirm("¿Está seguro de borrar el registro "+user_id+"?");                
+        if (respuesta) {            
+            $.ajax({
+              url: "bd/crud.php",
+              type: "POST",
+              datatype:"json",    
+              data:  {opcion:opcion, user_id:user_id},    
+              success: function() {
+                  tablaUsuarios.row(fila.parents('tr')).remove().draw();                  
+               }
+            });	
+        }
+     });
+         
+    });    
